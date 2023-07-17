15fc0f6ca909a5602479687f32991a7307c9eb2b
taturl <taturl@github.com>
2023-07-17 22:46:39 +0300

    Refactor code so that map does not re-render after each correct guess

--- kuntapeli.py
+++ kuntapeli.py
@@ -5,52 +5,74 @@ from streamlit_folium import st_folium
 from shapely.geometry import shape
 import json
 
-
-prompt = str(st.text_input('Kirjoita kunnan nimi ja paina enter. Niitä on yhteensä 309.'))
-
-if 'correct_quesses' not in st.session_state:
-    st.session_state['correct_quesses'] = []
-
-if 'kuntarajat' not in st.session_state:
-    st.session_state['kuntarajat'] = json.loads(open('Kuntarajat.geojson').read())['features']
-
-if 'm' not in st.session_state:
-    st.session_state['m'] = folium.Map(
-        location=[65.192059,  24.945831],
-        zoom_start=5,
-        tiles="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
-        attr='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+@st.cache_data
+def load_geojson_to_memory():
+    return(
+        json.loads(open('Kuntarajat.geojson').read())['features']
     )
 
-    def style_function(x): return {'fillOpacity': 0, 'weight': 0.1}
+KUNTARAJAT = load_geojson_to_memory()
 
-    folium.GeoJson(
-        'Kuntarajat.geojson',
-        name='geojson',
-        style_function=style_function
-    ).add_to(st.session_state.m)
+if 'correct_guesses' not in st.session_state:
+    st.session_state['correct_guesses'] = []
 
-if prompt:
-    prompt = prompt.casefold().capitalize()
-    names = [kunta['properties']['Name'] for kunta in st.session_state.kuntarajat]
+if 'fg' not in st.session_state:
+    st.session_state['fg'] = folium.FeatureGroup(name="Kuntarajat")
+
+def check_guess():
+    prompt = st.session_state.prompt.casefold().capitalize()
+    names = [kunta['properties']['Name'] for kunta in KUNTARAJAT]
     if prompt in names:
-        if prompt in st.session_state.correct_quesses:
-            st.write(prompt, '...on kunta ja tiesit sen jo.', )
+        if prompt in st.session_state.correct_guesses:
+            st.write(prompt, '...on kunta ja kirjoitit sen jo :-)', )
         else:
-            st.write(prompt, '...on kunta.', )
-        
-            # Add to results
-            st.session_state.correct_quesses.append(prompt)
-            print(st.session_state.correct_quesses)
+            st.write(prompt, '...on kunta. Lisätään se kartalle!', )
+            st.session_state.correct_guesses.append(prompt)
 
-            # Add all known kuntas to map
-            # for known_kunta in st.session_state.correct_quesses:
-            kunta = next(kunta for kunta in st.session_state.kuntarajat if kunta['properties']['Name'] == prompt)
+            kunta = next(kunta for kunta in KUNTARAJAT if kunta['properties']['Name'] == prompt)
             kunta_geo = shape(kunta["geometry"])
-            folium.GeoJson(kunta_geo, tooltip=prompt).add_to(st.session_state.m)
+            st.session_state.fg.add_child( 
+                folium.GeoJson(kunta_geo, tooltip=prompt)
+            )
+    else:
+        st.write(prompt, '... ei ole kunta.')
 
-if len(st.session_state.correct_quesses) >= 309:
+st.text_input(
+    'Suomessa on 309 kuntaa. Muistatko ne kaikki?',
+    key='prompt',
+    placeholder='Kirjoita tähän kunnannimi',
+    on_change=check_guess
+)
+
+if len(st.session_state.correct_guesses) >= 309:
     st.title("TIESIT KAIKKI KUNNAT! ONNEKSI OLKOON")
 
-# folium.LayerControl().add_to(st.session_state.m)
-st_data = st_folium(st.session_state.m, width=725)
+m = folium.Map(
+    location=[65.192059,  24.945831],
+    zoom_start=5,
+    tiles="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
+    attr='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+)
+
+def style_function(x): return {'fillOpacity': 0, 'weight': 0.1}
+
+# folium.GeoJson(
+#     'Kuntarajat.geojson',
+#     name='geojson',
+#     style_function=style_function
+# ).add_to(m)
+
+# if len(st.session_state.correct_guesses) >= len(fg):
+#     prompt = st.session_state.prompt.casefold().capitalize()
+#     kunta = next(kunta for kunta in KUNTARAJAT if kunta['properties']['Name'] == prompt)
+#     kunta_geo = shape(kunta["geometry"])
+#     fg.add_child( 
+#         folium.GeoJson(kunta_geo, tooltip=prompt)
+#     )
+
+out = st_folium(
+    m,
+    feature_group_to_add=st.session_state.fg,
+    height=600,
+    use_container_width=True
+)
